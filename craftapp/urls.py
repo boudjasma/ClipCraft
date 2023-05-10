014afb7757eb08a556b2ca5d14eca221e577bba0
Asma Boudjatat <asma.boudjatat@gmail.com>
2023-05-10 15:42:57 +0200

    authentification part 1

--- craftapp/urls.py
+++ craftapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from craftapp.views import HomeView
+from craftapp.views import HomeView, CustomLoginView, CustomLogoutView, SignUpView
+from django.conf.urls import include, url
+
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', HomeView.as_view()),
-]
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(template_name = 'craftapp/logout.html'), name='logout'),
+    path('signup/', SignUpView.as_view(template_name = 'craftapp/signup.html'), name='signup'),
+    path('accounts/profile/', HomeView.as_view(template_name="craftapp/home.html"), name="home"),
+] 
--- craftapp/views.py
+++ craftapp/views.py
@@ -1,4 +1,52 @@
+from django.shortcuts import redirect, render
 from django.views.generic import *
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
+class CustomLoginView(LoginView):
+    template_name = 'craftapp/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'craftapp/logout.html'
+    next_page = 'login'
+
+class SignUpView(View):
+    form_class = UserCreationForm
+    template_name = 'craftapp/signup.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
+            user_created = User.objects.create_user(username=username, password=password, email=email)
+            user_created.save()
+            user = authenticate(username=username, password=password)
+            
+            login(request, user)
+            return redirect('home')
+        return render(request, self.template_name, {'form': form})
+    
 class HomeView(TemplateView):
-    template_name = "craftapp/home.html"
+    template_name = 'craftapp/home.html'
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            title = request.POST.get('title')
+            text = request.POST.get('text')
+            selected_photo = request.POST.get('photo')
+            print(title, text, selected_photo)
+            # traitement de données se fait ici
+            context = {'title': title, 'text': text, 'selected_photo': selected_photo}
+            
+            return render(request, 'craftapp/result.html', context)
+
+
+
